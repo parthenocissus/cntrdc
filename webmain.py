47d1a20e0a75635443d29abb960882cae48ba88e
parthenocissus <uros.krcadinac@gmail.com>
2021-02-03 16:33:05 +0100

    chess

--- webmain.py
+++ webmain.py
@@ -1,4 +1,4 @@
-# Uros Krcadinac 2020
+# Uros Krcadinac 2021
 # Main App
 
 import sys
@@ -21,22 +21,14 @@ freezer = Freezer(app)
 app.config.from_object(__name__)
 
 en = LangUtilEn(fp)
-sh = LangUtilSh(fp)
-
 
 # BASE ROUTES
-# homepages in english and serbian
+# homepage in english
 
-
-@app.route("/work/")
 @app.route("/")
 def home():
     return render_template('home.html', params=en.params())
 
-
-@app.route("/rad/")
-def home_s():
-    return render_template('home.html', params=sh.params())
 
 
 # PROJECT LIST SECTION
@@ -48,12 +40,6 @@ def projects():
     return render_template('project_list.html', projects=project_list, params=en.params(), data=data)
 
 
-@app.route("/rad/projekti/")
-def projects_s():
-    project_list, data = utils.projects(fp, sh)
-    return render_template('project_list.html', projects=project_list, params=sh.params(), data=data)
-
-
 # PROJECTS FILTERED
 # rendering filtered list of projects, also project_list.html
 
@@ -61,12 +47,6 @@ def projects_s():
 def projects_by_category(by, criteria):
     list, data = utils.projects_by_category(fp, en, by, criteria)
     return render_template('project_list.html', projects=list, params=en.params(), data=data)
-
-
-@app.route("/rad/projekti/<by>/<criteria>")
-def projects_by_category_s(by, criteria):
-    list, data = utils.projects_by_category(fp, sh, by, criteria)
-    return render_template('project_list.html', projects=list, params=sh.params(), data=data)
 
 
 # PROJECT SECTION
@@ -78,11 +58,6 @@ def project(name):
     return render_template('project.html', project=this_project, params=en.params())
 
 
-@app.route('/rad/projekti/<name>/')
-def project_s(name):
-    this_project = utils.project(fp, sh, name)
-    return render_template('project.html', project=this_project, params=sh.params())
-
 
 # WRITING SECTION
 # texts and books
@@ -91,12 +66,6 @@ def project_s(name):
 def writing():
     page, data = utils.writing(fp, en)
     return render_template('page.html', params=en.params(), page=page, data=data)
-
-
-@app.route('/rad/tekstovi/')
-def writing_s():
-    page, data = utils.writing(fp, sh)
-    return render_template('page.html', params=sh.params(), page=page, data=data)
 
 
 # TEACHING SECTION
@@ -108,12 +77,6 @@ def teaching():
     return render_template('page.html', params=en.params(), page=page, data=data)
 
 
-@app.route('/rad/edukacija/')
-def teaching_s():
-    page, data = utils.teaching(fp, sh)
-    return render_template('page.html', params=sh.params(), page=page, data=data)
-
-
 # SPEAKING SECTION
 # talks and public lectures
 
@@ -121,12 +84,6 @@ def teaching_s():
 def speaking():
     page, data = utils.speaking(fp, en)
     return render_template('page.html', params=en.params(), page=page, data=data)
-
-
-@app.route('/rad/nastupi/')
-def speaking_s():
-    page, data = utils.speaking(fp, sh)
-    return render_template('page.html', params=sh.params(), page=page, data=data)
 
 
 # PRESS SECTION
@@ -138,12 +95,6 @@ def press():
     return render_template('page.html', params=en.params(), page=page, data=data)
 
 
-@app.route('/rad/mediji/')
-def press_s():
-    page, data = utils.press(fp, sh)
-    return render_template('page.html', params=sh.params(), page=page, data=data)
-
-
 # NOTE LIST SECTION
 # rendering note_list.html
 
@@ -151,55 +102,6 @@ def press_s():
 def note_list():
     data = utils.note_list(fp, en)
     return render_template('note_list.html', params=en.params(), data=data)
-
-
-@app.route("/rad/sveska/")
-def note_list_s():
-    data = utils.note_list(fp, sh)
-    return render_template('note_list.html', params=sh.params(), data=data)
-
-
-
-# NOTES SECTION
-# rendering note.html
-
-@app.route('/work/notebook/<name>/')
-def note(name):
-    try:
-        this_note = utils.note(fp, en, name)
-    except:
-        data = utils.note_list(fp, en)
-        data["error"] = True
-        return render_template('note_list.html', params=en.params(), data=data)
-    else:
-        return render_template('note.html', project=this_note, params=en.params())
-
-
-@app.route('/rad/sveska/<name>/')
-def note_s(name):
-    try:
-        this_note = utils.note(fp, sh, name)
-    except:
-        data = utils.note_list(fp, sh)
-        data["error"] = True
-        return render_template('note_list.html', params=sh.params(), data=data)
-    else:
-        return render_template('note.html', project=this_note, params=sh.params())
-
-
-# TEMPORARY NOTES SECTION
-# rendering note.html
-
-@app.route('/work/notebook/tmp/<name>/')
-def note_tmp(name):
-    this_note = utils.note_tmp(fp, en, name)
-    return render_template('note.html', project=this_note, params=en.params())
-
-
-@app.route('/rad/sveska/tmp/<name>/')
-def note_tmp_s(name):
-    this_note = utils.note_tmp(fp, sh, name)
-    return render_template('note.html', project=this_note, params=sh.params())
 
 
 
@@ -212,28 +114,7 @@ def about():
     return render_template('about.html', params=en.params(), page=page, data=data)
 
 
-@app.route('/rad/bio/')
-def about_s():
-    page, data = utils.about(fp, sh)
-    return render_template('about.html', params=sh.params(), page=page, data=data)
 
-
-
-# SPACE ROUTE
-# for specific project pages
-
-@app.route('/space/<name>/')
-def space(name):
-    return render_template('/space/' + name + '.html')
-
-
-
-# ADDITIONAL ROUTES
-# synesketch, download, etc.
-
-@app.route('/synesketch/')
-def synesketch():
-    return redirect("/work/projects/synesketch", code=302)
 
 
 @app.route('/download/<path:args>')
@@ -247,10 +128,6 @@ def download_file(args=None):
     return send_file(path)
 
 
-# @app.route('/3/')
-# def poem():
-#     return render_template('test/3.html')
-
 
 # MAIN
 # starting app
@@ -259,6 +136,4 @@ if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "build":
         freezer.freeze()
     else:
-        # app.run(host='0.0.0.0', debug=True)
         app.run(host='127.0.0.1', debug=True)
-        # app.run(host='0.0.0.0')
